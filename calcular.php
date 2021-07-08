8fa3519ece3763b9e18090f1ed47a99298a0ffa9
Angel Iglesias <angel.iglesias@utp.ac.pa>
2021-07-08 15:48:37 -0500

    modificaciones en calcular.php en issue2

---
+++ calcular.php
@@ -0,0 +1,32 @@
+<?php 
+$cantidad = $_POST['cantidad'];
+$subtotal = $cantidad*125;
+$numero_cajas = ceil($cantidad/4);
+$flete = $numero_cajas*50;
+if ($cantidad>1000)
+    $descuento=$subtotal*0.15;
+else
+{
+    if($cantidad>100)
+        $descuento=$subtotal*0.05;
+    else
+        $descuento=0;
+}
+$total=$subtotal+$flete-$descuento;
+echo "El precio total a pagar es: ".$total."<br>";
+echo "El monto por flete es: ".$flete."<br>";
+echo "El descuento aplicado fue: ".$descuento;
+$edad = 24;
+switch($edad)
+{
+    case 12:
+        echo "La mitad de tu edad";
+        break;
+    case 24:
+        echo "Esta es tu edad";
+        break;
+    case 48:
+        echo "El doble de tu edad";
+        break;
+}
+?>
